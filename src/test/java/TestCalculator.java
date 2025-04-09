7a1ec41505c2f31812a17afc8739f5793bb49387
sieuta <luantqps39694@gmail.com>
2025-04-09 19:37:06 +0700

    Đây là test fail

--- src/test/java/TestCalculator.java
+++ src/test/java/TestCalculator.java
@@ -43,5 +43,11 @@ public class TestCalculator {
         calculator.div(0);
         Assert.assertEquals(0, calculator.getResult());
     }
+    @Test
+    public void testFail() {
+        calculator.plus(100);
+        calculator.div(0);
+        Assert.assertEquals(10, calculator.getResult());
+    }
 }
 
